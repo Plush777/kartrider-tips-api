f2caadc8366fc13b4c20be71badbf4a47065414d
서규영 <jjseo7601@naver.com>
2024-03-31 18:45:03 +0900

    exp

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const express = require('express');
 const axios = require("axios");
 const cheerio = require("cheerio");
-const log = console.log;
 
-const getHtml = async () => {
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', async (req, res) => {
     try {
-        return await axios.get("https://kartdrift.nexon.com/kartdrift/ko/news/announcement/list?searchKeywordType=THREAD_TITLE&keywords=%EC%BF%A0%ED%8F%B0");
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-getHtml()
-    .then(html => {
+        const html = await axios.get("https://kartdrift.nexon.com/kartdrift/ko/news/announcement/list?searchKeywordType=THREAD_TITLE&keywords=%EC%BF%A0%ED%8F%B0");
         let ulList = [];
         const $ = cheerio.load(html.data);
         const $bodyList = $(".board_list ul").children("li");
 
-        $bodyList.each(function(i, elem) {
+        $bodyList.each(function (i, elem) {
             ulList[i] = {
                 title: $(this).find('.tit span').text(),
                 date: $(this).find('.info .date').text(),
@@ -24,6 +24,9 @@ getHtml()
             };
         });
 
-        return ulList;
-    })
-    .then(res => log(res));
+        res.json(ulList);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
