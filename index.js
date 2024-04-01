9c4659b4911d25314ca60c35d5725f15fb4096e1
Plush777 <sky11916@naver.com>
2024-04-01 11:30:45 +0900

    240401

--- index.js
+++ index.js
@@ -11,25 +11,56 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', async (req, res) => {
-    try {
-        const html = await axios.get("https://kartdrift.nexon.com/kartdrift/ko/news/announcement/list?searchKeywordType=THREAD_TITLE&keywords=%EC%BF%A0%ED%8F%B0");
-        let ulList = [];
-        const $ = cheerio.load(html.data);
-        const $bodyList = $(".board_list ul").children("li");
+/* 공지사항에서 제목 검색 => 쿠폰에 대한 데이터 */
+const NEWS_COUPON_URL = "https://kartdrift.nexon.com/kartdrift/ko/news/announcement/list?searchKeywordType=THREAD_TITLE&keywords=%EC%BF%A0%ED%8F%B0";
+/* 소셜 이벤트에서 내용 검색 => 쿠폰에 대한 데이터 */
+const CM_EVENT_COUPON_URL = "https://kartdrift.nexon.com/kartdrift/ko/news/communityevent/list?searchKeywordType=THREAD_CONTENT&keywords=%EC%BF%A0%ED%8F%B0";
 
-        $bodyList.each(function (i, elem) {
-            ulList[i] = {
-                title: $(this).find('.tit span').text(),
-                date: $(this).find('.info .date').text(),
-                view: $(this).find('.view').text()
-            };
+const getHtml = async (url, resource, response, selector) => {
+    try {
+        const html = await axios.get(url);
+        const $ = cheerio.load(html.data);
+
+        let list = [];
+        let object;
+        let $bodyList = $(selector);
+
+        $bodyList.each(function (i) {
+            switch (resource) {
+                case "news":
+                case "cm_event":
+                    object = {
+                        title: $(this).find('.tit span').text(),
+                        date: $(this).find('.info .date').text(),
+                        view: $(this).find('.view').text()
+                    }
+    
+                    break;
+    
+                default:
+                    break;
+            }
+
+            list[i] = object;
         });
 
-        res.json(ulList);
+        response.json(list);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        response.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+app.get('/api/coupon/:resource', (req, res) => {
+    let { resource } = req.params;
+
+    switch (resource) {
+        case "news": 
+            getHtml(NEWS_COUPON_URL, resource, res, ".board_list ul li");
+            break;
+        case "cm_event":
+            getHtml(CM_EVENT_COUPON_URL, resource, res, ".board_list ul li");
+            break;
     }
 });
 
