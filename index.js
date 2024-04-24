d7506b84bd70b8dae8997b96f861dba37d509068
Plush777 <sky11916@naver.com>
2024-04-24 11:49:30 +0900

    랭킹 데이터 추가

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const KART_LIST_URL = "https://kartdrift.nexon.com/kartdrift/ko/guide/gameguide/
 /* 카트 개발자노트 게시글 */
 const DEV_NOTE_URL = "https://kartdrift.nexon.com/kartdrift/ko/news/announcement/list?searchKeywordType=THREAD_TITLE&keywords=%EA%B0%9C%EB%B0%9C%EC%9E%90%EB%85%B8%ED%8A%B8"
 /* 카트 업데이트 게시글 */
-const UPDATE_URL = "https://kartdrift.nexon.com/kartdrift/ko/news/update/list"
+const UPDATE_URL = "https://kartdrift.nexon.com/kartdrift/ko/news/update/list";
+/* 게임 순위 */
+const RANKING_URL = "https://www.gamemeca.com/ranking.php";
 
 /* 
     https://blog.ssogari.dev/25
@@ -89,23 +91,14 @@ const getHtml = async (url, resource, response, selector, condition) => {
             });
         } else if (condition === 'kart') {
             $bodyList.each(function (i, item) {
-
-                /* 
-                    1. kartName을 전부 가져옵니다.
-                    2. 하나의 문자열로 된 데이터에서 split, 정규식을 사용해서 prefix를 기준으로 문자열들을 분리하고 배열로 만듭니다.
-                    3. 분리시킨 배열 아이템들 중에서 공백이 없는 아이템들만 filter로 걸러냅니다.
-                    4. " 연습카트" << 이런식의 텍스트 공백을 정규식으로 제거합니다.
-                */
                 let kartNames = $(this).find('tr td[style*="background-color:black"] span[style*="letter-spacing:-1.0pt"]').text();
+
                 let kartNamePrefix = kartNames.split(/\[일반\]|\[희귀\]|\[고급\]|\[영웅\]|\[전설\]/);
                 let kartNameSplitArray = kartNamePrefix.filter((name) => name.trim() !== "");
                 let kartNameTrimmedArray = kartNameSplitArray.map((name) => name.replace(/^ /, ''));
-
-                /* kartType 문자열을 가져옵니다. 얘는 구분자가 없기 때문에 match랑 정규식을 사용해 문자열을 분리하여 배열로 받습니다.
-                 
-                    *match: 문자열에서 정규식과 일치하는 부분을 찾아 배열로 반환.
-                */
+              
                 const kartTypeSelector = 'tr td[style*="background-color:#9a68f4"] span[style*="letter-spacing:-1.0pt"], tr td[style*="background-color:#ee6060"] span[style*="letter-spacing:-1.0pt"], tr td[style*="background-color:#6b72fb"] span[style*="letter-spacing:-1.0pt"]'
+                
                 let kartTypes = $(this).find(kartTypeSelector).text();
                 let kartTypeStringMatchArray = kartTypes.match(/밸런스형|속도형|드리프트형/g);
 
@@ -114,6 +107,16 @@ const getHtml = async (url, resource, response, selector, condition) => {
                     type: kartTypeStringMatchArray
                 }
                 
+                list[i] = object;
+            });
+        } else if (condition === 'ranking') {
+            $bodyList.each(function (i, item) {
+                object = {
+                    title: $(this).find('.game-name a').text(),
+                    rankChange: $(this).find('.rankChange').text(),
+                    rankStatus: $(this).find('.rankChange .ranking-static-img').attr('class')
+                }
+
                 list[i] = object;
             });
         }
@@ -207,6 +210,10 @@ app.get('/api/chzzk/:info', (req, res) => {
             res.status(404).json({ error: 'Not Found' });
             break;
     }
+});
+
+app.get('/api/ranking', (req, res) => {
+    getHtml(RANKING_URL, null, res, ".ranking-table tbody .ranking-table-rows", "ranking");
 });
 
 module.exports = app;
